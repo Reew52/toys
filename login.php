8647f14d5900d0840e6cca74bf70d2b44845b659
Reew52 <koydzxxx@gmail.com>
2022-10-31 20:16:35 +0700

    upload 2

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
 </head>
 <body>
 	<?php 
-$connect = mysqli_connect('localhost', 'root', '', 'music');
+$connect = mysqli_connect('3.132.234.157','rew52','123@123a','musics');
 if($connect){
 	echo "";
 }else{
